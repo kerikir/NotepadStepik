83db00c0c1b137d4985b92dd0105f102cd046185
kerikir <vlasov-kerik@mail.ru>
2026-03-30 17:46:50 +0300

    added the repository in use cases

--- app/src/main/java/com/kerikir/notes/domain/DeleteNoteUseCase.kt
+++ app/src/main/java/com/kerikir/notes/domain/DeleteNoteUseCase.kt
@@ -1,8 +1,10 @@
 package com.kerikir.notes.domain
 
-class DeleteNoteUseCase {
+class DeleteNoteUseCase(
+    private val repository: NotesRepository
+) {
 
     operator fun invoke(noteId: Int) {
-        TODO()
+        repository.deleteNote(noteId)
     }
 }
--- app/src/main/java/com/kerikir/notes/domain/GetAllNotesUseCase.kt
+++ app/src/main/java/com/kerikir/notes/domain/GetAllNotesUseCase.kt
@@ -3,9 +3,11 @@ package com.kerikir.notes.domain
 import kotlinx.coroutines.flow.Flow
 
 
-class GetAllNotesUseCase {
+class GetAllNotesUseCase(
+    private val repository: NotesRepository
+) {
 
     operator fun invoke(): Flow<List<Note>> {
-        TODO()
+        return repository.getAllNotes()
     }
 }
