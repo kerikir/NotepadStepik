f80d3b60e115297be3e99d80ec8ec625ffe63b37
kerikir <vlasov-kerik@mail.ru>
2026-03-30 16:18:38 +0300

    changed usecase get note

--- app/src/main/java/com/kerikir/notes/domain/GetNoteUseCase.kt
+++ app/src/main/java/com/kerikir/notes/domain/GetNoteUseCase.kt
@@ -1,10 +1,8 @@
 package com.kerikir.notes.domain
 
-import kotlinx.coroutines.flow.Flow
-
 class GetNoteUseCase {
 
-    operator fun invoke(id: Int): Flow<Note> {
+    operator fun invoke(noteId: Int): Note {
         TODO()
     }
 }
