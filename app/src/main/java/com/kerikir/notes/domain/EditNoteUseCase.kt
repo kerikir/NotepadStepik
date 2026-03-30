a0ddd89156901f4ee8e7dca280fb7fcad609b898
kerikir <vlasov-kerik@mail.ru>
2026-03-30 16:16:30 +0300

    changed usecase edit note

--- app/src/main/java/com/kerikir/notes/domain/EditNoteUseCase.kt
+++ app/src/main/java/com/kerikir/notes/domain/EditNoteUseCase.kt
@@ -1,10 +1,8 @@
 package com.kerikir.notes.domain
 
-import kotlinx.coroutines.flow.Flow
-
 class EditNoteUseCase {
 
-    operator fun invoke(): Flow<Note> {
+    operator fun invoke(note: Note) {
         TODO()
     }
 }
