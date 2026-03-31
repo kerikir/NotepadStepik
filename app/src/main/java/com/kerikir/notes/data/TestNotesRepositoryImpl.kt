a9768ebbd414e4e76362fae5818afd7d6fdacbe7
kerikir <vlasov-kerik@mail.ru>
2026-03-31 20:16:06 +0300

    added the implementation interface repository in the data layer

---
+++ app/src/main/java/com/kerikir/notes/data/TestNotesRepositoryImpl.kt
@@ -0,0 +1,36 @@
+package com.kerikir.notes.data
+
+import com.kerikir.notes.domain.Note
+import com.kerikir.notes.domain.NotesRepository
+import kotlinx.coroutines.flow.Flow
+
+class TestNotesRepositoryImpl : NotesRepository {
+    
+    override fun addNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteNote(noteId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editNote(note: Note) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllNotes(): Flow<List<Note>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNote(noteId: Int): Note {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchNotes(query: String): Flow<List<Note>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun switchPinnedStatus(noteId: Int) {
+        TODO("Not yet implemented")
+    }
+}
