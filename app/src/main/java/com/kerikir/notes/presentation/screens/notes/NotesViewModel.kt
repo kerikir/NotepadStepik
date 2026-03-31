4e8f609564da26176777deb3b7d18d8808bc28b5
kerikir <vlasov-kerik@mail.ru>
2026-03-31 23:47:46 +0300

    added the use cases in the ViewModel

--- app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesViewModel.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesViewModel.kt
@@ -1,6 +1,26 @@
 package com.kerikir.notes.presentation.screens.notes
 
 import androidx.lifecycle.ViewModel
+import com.kerikir.notes.data.TestNotesRepositoryImpl
+import com.kerikir.notes.domain.AddNoteUseCase
+import com.kerikir.notes.domain.DeleteNoteUseCase
+import com.kerikir.notes.domain.EditNoteUseCase
+import com.kerikir.notes.domain.GetAllNotesUseCase
+import com.kerikir.notes.domain.GetNoteUseCase
+import com.kerikir.notes.domain.SearchNotesUseCase
+import com.kerikir.notes.domain.SwitchPinnedStatusUseCase
+
 
 class NotesViewModel : ViewModel() {
+
+    private val repository = TestNotesRepositoryImpl
+
+    private val getAllNotesUseCase = GetAllNotesUseCase(repository)
+    private val searchNotesUseCase = SearchNotesUseCase(repository)
+    private val switchPinnedStatusUseCase = SwitchPinnedStatusUseCase(repository)
+
+    private val addNoteUseCase = AddNoteUseCase(repository)
+    private val editNoteUseCase = EditNoteUseCase(repository)
+    private val deleteNoteUseCase = DeleteNoteUseCase(repository)
+    private val getNoteUseCase = GetNoteUseCase(repository)
 }
