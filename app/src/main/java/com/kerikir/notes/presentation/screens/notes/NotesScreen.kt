4a12fbd2ac6f69a9f53385f72c3425504e6d754a
kerikir <vlasov-kerik@mail.ru>
2026-04-02 00:56:59 +0300

    added the LazyRow for the elements collection

--- app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
@@ -33,14 +34,14 @@ fun NotesScreen(
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
 
+
         item {
-            Row(
+            LazyRow(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .horizontalScroll(rememberScrollState()),
+                    .fillMaxWidth(),
                 horizontalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                state.pinnedNotes.forEach {  note ->
+                items(state.pinnedNotes) { note ->
                     NotesCard(
                         note = note,
                         onNoteClick = { note ->
