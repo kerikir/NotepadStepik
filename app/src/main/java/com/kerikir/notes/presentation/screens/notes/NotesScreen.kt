ccee2b94a6a64ec41008b732ef2bf07216ea8c56
kerikir <vlasov-kerik@mail.ru>
2026-04-01 15:36:48 +0300

    added the text components for the list notes

--- app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
@@ -1,11 +1,33 @@
 package com.kerikir.notes.presentation.screens.notes
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun NotesScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: NotesViewModel = viewModel()
 ) {
 
+    val state = viewModel.state.collectAsState()
+
+    val currentState = state.value
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        currentState.otherNotes.forEach { note ->
+            Text(
+                text = "${note.title} - ${note.content}",
+                fontSize = 24.sp
+            )
+        }
+    }
 }
