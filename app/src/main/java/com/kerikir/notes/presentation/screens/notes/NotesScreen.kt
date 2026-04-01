b94fdf3f76911a54b465539d604225cce16e3528
kerikir <vlasov-kerik@mail.ru>
2026-04-01 16:34:56 +0300

    added the delegate getValue

--- app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -21,9 +23,7 @@ fun NotesScreen(
     viewModel: NotesViewModel = viewModel()
 ) {
 
-    val state = viewModel.state.collectAsState()
-
-    val currentState = state.value
+    val state by viewModel.state.collectAsState()
 
     val scrollState = remember {
         ScrollState(0)
@@ -35,7 +35,7 @@ fun NotesScreen(
             .verticalScroll(scrollState),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        currentState.otherNotes.forEach { note ->
+        state.otherNotes.forEach { note ->
             Text(
                 modifier = Modifier.clickable {
                     viewModel.processCommand(NotesCommand.EditNote(note))
