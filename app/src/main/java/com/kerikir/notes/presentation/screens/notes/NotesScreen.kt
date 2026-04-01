fec6bd8f77451831a591e393684d3535616023ac
kerikir <vlasov-kerik@mail.ru>
2026-04-01 15:13:22 +0300

    added the composable function NotesScreen

---
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
@@ -0,0 +1,11 @@
+package com.kerikir.notes.presentation.screens.notes
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NotesScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
