573a3586da33cb3f3a29e832c59156b6c55611e5
kerikir <vlasov-kerik@mail.ru>
2026-04-02 00:32:36 +0300

    added the LazyColumn

--- app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
@@ -1,6 +1,5 @@
 package com.kerikir.notes.presentation.screens.notes
 
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -8,13 +7,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,20 +28,33 @@ fun NotesScreen(
 
     val state by viewModel.state.collectAsState()
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .padding(top = 48.dp)
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .horizontalScroll(rememberScrollState()),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            state.pinnedNotes.forEach {  note ->
+        item {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .horizontalScroll(rememberScrollState()),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                state.pinnedNotes.forEach {  note ->
+                    NotesCard(
+                        note = note,
+                        onNoteClick = { note ->
+                            viewModel.processCommand(NotesCommand.SwitchPinnedStatus(note.id))
+                        }
+                    )
+                }
+            }
+        }
+
+        state.otherNotes.forEach { note ->
+            item {
                 NotesCard(
                     note = note,
                     onNoteClick = { note ->
@@ -50,15 +62,6 @@ fun NotesScreen(
                     }
                 )
             }
-        }
-
-        state.otherNotes.forEach { note ->
-            NotesCard(
-                note = note,
-                onNoteClick = { note ->
-                    viewModel.processCommand(NotesCommand.SwitchPinnedStatus(note.id))
-                }
-            )
         }
     }
 }
