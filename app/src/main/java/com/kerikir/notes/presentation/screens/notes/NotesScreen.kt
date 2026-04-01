51384c77aebc752940996861101ec4249d9a9f63
kerikir <vlasov-kerik@mail.ru>
2026-04-01 15:46:34 +0300

    added scroll for the list notes

--- app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
@@ -1,7 +1,10 @@
 package com.kerikir.notes.presentation.screens.notes
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -21,6 +24,9 @@ fun NotesScreen(
     val currentState = state.value
 
     Column(
+        modifier = Modifier
+            .padding(top = 48.dp)
+            .verticalScroll(ScrollState(0)),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         currentState.otherNotes.forEach { note ->
