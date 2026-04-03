47e2947a8dbe56720ff8d30dfe8c89485bc98c24
kerikir <vlasov-kerik@mail.ru>
2026-04-03 13:59:25 +0300

    added the composable function for the Title

--- app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -62,6 +64,22 @@ fun NotesScreen(
             }
         }
     }
+}
+
+
+
+@Composable
+private fun Title(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onBackground
+    )
 }
 
 
