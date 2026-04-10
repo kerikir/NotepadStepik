1aaa1b69c2ca41a3da9a05774a1d5705f215b235
kerikir <vlasov-kerik@mail.ru>
2026-04-10 19:58:20 +0300

    added the TopAppBar

--- app/src/main/java/com/kerikir/notes/presentation/screens/creation/CreateNoteScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/creation/CreateNoteScreen.kt
@@ -1,7 +1,22 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.kerikir.notes.presentation.screens.creation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -9,5 +24,41 @@ fun CreateNoteScreen(
     modifier: Modifier = Modifier,
     viewModel: CreateNoteViewModel = viewModel()
 ) {
-    
+
+    val state = viewModel.state.collectAsState()
+    val currentState = state.value
+
+    when(currentState) {
+        is CreateNoteState.Creation -> {
+            Scaffold(
+                modifier = modifier,
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = "Create Note",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        },
+                        navigationIcon = {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = "Back"
+                            )
+                        }
+                    )
+                }
+            ) { innerPadding ->
+                Column(
+                    modifier = Modifier.padding(innerPadding)
+                ) {
+
+                }
+            }
+        }
+
+        CreateNoteState.Finished -> {}
+    }
 }
