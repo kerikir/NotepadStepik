6f34ad1e3b006733570432eae33604f047e18214
kerikir <vlasov-kerik@mail.ru>
2026-04-10 19:40:07 +0300

    added the composable function CreateNoteScreen

---
+++ app/src/main/java/com/kerikir/notes/presentation/screens/creation/CreateNoteScreen.kt
@@ -0,0 +1,13 @@
+package com.kerikir.notes.presentation.screens.creation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun CreateNoteScreen(
+    modifier: Modifier = Modifier,
+    viewModel: CreateNoteViewModel = viewModel()
+) {
+    
+}
