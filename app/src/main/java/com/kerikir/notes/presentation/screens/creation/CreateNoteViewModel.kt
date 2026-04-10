e144f38b8cc713b74a6422fe62ee86247ba6364f
kerikir <vlasov-kerik@mail.ru>
2026-04-10 15:27:20 +0300

    added the initial state in the Create Note ViewModel

--- app/src/main/java/com/kerikir/notes/presentation/screens/creation/CreateNoteViewModel.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/creation/CreateNoteViewModel.kt
@@ -1,8 +1,18 @@
 package com.kerikir.notes.presentation.screens.creation
 
 import androidx.lifecycle.ViewModel
+import com.kerikir.notes.data.TestNotesRepositoryImpl
+import com.kerikir.notes.domain.AddNoteUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class CreateNoteViewModel: ViewModel() {
+
+    private val repository = TestNotesRepositoryImpl
+    private val addNoteUseCase = AddNoteUseCase(repository)
+
+    private val _state = MutableStateFlow<CreateNoteState>(CreateNoteState.Creation())
+    val state = _state.asStateFlow()
 }
 
 
@@ -21,9 +31,9 @@ sealed interface CreateNoteCommand {
 sealed interface CreateNoteState {
 
     data class Creation(
-        val title: String,
-        val content: String,
-        val isSaveEnabled: Boolean
+        val title: String = "",
+        val content: String = "",
+        val isSaveEnabled: Boolean = false
     ) : CreateNoteState
 
     data object Finished: CreateNoteState
