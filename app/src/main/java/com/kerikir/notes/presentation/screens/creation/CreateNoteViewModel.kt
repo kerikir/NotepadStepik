1766506f4fe4f897e0630e7bb9dbcf62b85d5bf7
kerikir <vlasov-kerik@mail.ru>
2026-04-10 15:15:43 +0300

    added the sealed interface CreateNoteState

--- app/src/main/java/com/kerikir/notes/presentation/screens/creation/CreateNoteViewModel.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/creation/CreateNoteViewModel.kt
@@ -4,3 +4,15 @@ import androidx.lifecycle.ViewModel
 
 class CreateNoteViewModel: ViewModel() {
 }
+
+
+sealed interface CreateNoteState {
+
+    data class Creation(
+        val title: String,
+        val content: String,
+        val isSaveEnabled: Boolean
+    ) : CreateNoteState
+
+    data object Finished: CreateNoteState
+}
