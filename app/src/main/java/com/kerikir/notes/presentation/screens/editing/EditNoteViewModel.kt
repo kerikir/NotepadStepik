18db8559a89547c9469bac6493be42f1391f901d
kerikir <vlasov-kerik@mail.ru>
2026-04-11 12:57:25 +0300

    added the sealed interface EditNoteState

--- app/src/main/java/com/kerikir/notes/presentation/screens/editing/EditNoteViewModel.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/editing/EditNoteViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kerikir.notes.data.TestNotesRepositoryImpl
 import com.kerikir.notes.domain.AddNoteUseCase
+import com.kerikir.notes.domain.Note
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -14,37 +15,37 @@ class EditNoteViewModel: ViewModel() {
     private val repository = TestNotesRepositoryImpl
     private val addNoteUseCase = AddNoteUseCase(repository)
 
-    private val _state = MutableStateFlow<CreateNoteState>(CreateNoteState.Creation())
+    private val _state = MutableStateFlow<EditNoteState>(EditNoteState.Creation())
     val state = _state.asStateFlow()
 
     fun processCommand(command: EditNoteCommand) {
         when (command) {
             EditNoteCommand.Back -> {
-                _state.update { CreateNoteState.Finished }
+                _state.update { EditNoteState.Finished }
             }
 
             is EditNoteCommand.InputContent -> {
                 _state.update { previousState ->
-                    if (previousState is CreateNoteState.Creation) {
+                    if (previousState is EditNoteState.Creation) {
                         previousState.copy(
                             content = command.content,
                             isSaveEnabled = previousState.title.isNotBlank() && command.content.isNotBlank()
                         )
                     } else {
-                        CreateNoteState.Creation(content = command.content)
+                        EditNoteState.Creation(content = command.content)
                     }
                 }
             }
 
             is EditNoteCommand.InputTitle -> {
                 _state.update { previousState ->
-                    if (previousState is CreateNoteState.Creation) {
+                    if (previousState is EditNoteState.Creation) {
                         previousState.copy(
                             title = command.title,
                             isSaveEnabled = command.title.isNotBlank() && previousState.content.isNotBlank()
                         )
                     } else {
-                        CreateNoteState.Creation(title = command.title)
+                        EditNoteState.Creation(title = command.title)
                     }
                 }
             }
@@ -52,11 +53,11 @@ class EditNoteViewModel: ViewModel() {
             EditNoteCommand.Save -> {
                 viewModelScope.launch {
                     _state.update { previousState ->
-                        if (previousState is CreateNoteState.Creation) {
+                        if (previousState is EditNoteState.Creation) {
                             val title = previousState.title
                             val content = previousState.content
                             addNoteUseCase(title, content)
-                            CreateNoteState.Finished
+                            EditNoteState.Finished
                         } else {
                             previousState
                         }
@@ -82,13 +83,15 @@ sealed interface EditNoteCommand {
 }
 
 
-sealed interface CreateNoteState {
+sealed interface EditNoteState {
 
     data class Creation(
-        val title: String = "",
-        val content: String = "",
-        val isSaveEnabled: Boolean = false
-    ) : CreateNoteState
+       val note: Note
+    ) : {
 
-    data object Finished: CreateNoteState
+        val isSaveEnabled: Boolean
+            get() = note.title.isNotBlank() && note.content.isNotBlank()
+    }
+
+    data object Finished: EditNoteState
 }
