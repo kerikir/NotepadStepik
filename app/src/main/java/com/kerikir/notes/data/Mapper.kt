ba5a07955050a29bddd50ef5b63d283ee7fb9a70
kerikir <vlasov-kerik@mail.ru>
2026-04-12 23:02:29 +0300

    added the mapper for convert NoteDbModel to Note

--- app/src/main/java/com/kerikir/notes/data/Mapper.kt
+++ app/src/main/java/com/kerikir/notes/data/Mapper.kt
@@ -5,3 +5,8 @@ import com.kerikir.notes.domain.Note
 fun Note.toDbModel(): NoteDbModel {
     return NoteDbModel(id, title, content, updatedAt, isPinned)
 }
+
+
+fun NoteDbModel.toEntity(): Note {
+    return Note(id, title, content, updatedAt, isPinned)
+}
