819988beb2cd17a61fb50f639e5728fecec63668
kerikir <vlasov-kerik@mail.ru>
2026-04-13 14:51:32 +0300

    added queries to the database

--- app/src/main/java/com/kerikir/notes/data/NotesDao.kt
+++ app/src/main/java/com/kerikir/notes/data/NotesDao.kt
@@ -12,4 +12,10 @@ interface NotesDao {
 
     @Query("SELECT * FROM notes WHERE title LIKE '%' || :query || '%' OR content LIKE '%' || :query || '%' ORDER BY updatedAt DESC")
     fun searchNotes(query: String): Flow<List<NoteDbModel>>
+
+    @Query("DELETE FROM notes WHERE id == :noteId")
+    suspend fun deleteNote(noteId: Int)
+
+    @Query("UPDATE notes SET isPinned = NOT isPinned WHERE id == :noteId")
+    suspend fun switchPinnedStatus(noteId: Int)
 }
