ccee64b07f46613dcfe7fef35d5ee22e8964b185
kerikir <vlasov-kerik@mail.ru>
2026-04-13 20:42:34 +0300

    added the method getInstance in the class Database

--- app/src/main/java/com/kerikir/notes/data/NotesDatabase.kt
+++ app/src/main/java/com/kerikir/notes/data/NotesDatabase.kt
@@ -1,6 +1,8 @@
 package com.kerikir.notes.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
@@ -11,4 +13,27 @@ import androidx.room.RoomDatabase
 abstract class NotesDatabase : RoomDatabase() {
 
     abstract fun notesDao(): NotesDao
+
+    companion object {
+
+        private var instance: NotesDatabase? = null
+        private val LOCK = Any()
+
+        fun getInstance(context: Context): NotesDatabase {
+
+            instance?.let { return it }
+
+            synchronized(LOCK) {
+                instance?.let { return it }
+
+                return Room.databaseBuilder(
+                    context = context,
+                    klass = NotesDatabase::class.java,
+                    name = "notes.db"
+                ).build().also {
+                    instance = it
+                }
+            }
+        }
+    }
 }
