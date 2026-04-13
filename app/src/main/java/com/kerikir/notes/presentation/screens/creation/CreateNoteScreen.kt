6106e8639ba024f92accee9b204654a295a0298c
kerikir <vlasov-kerik@mail.ru>
2026-04-14 00:27:39 +0300

    added ViewModel creation on screens using factory

--- app/src/main/java/com/kerikir/notes/presentation/screens/creation/CreateNoteScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/creation/CreateNoteScreen.kt
@@ -2,7 +2,7 @@
 
 package com.kerikir.notes.presentation.screens.creation
 
-import android.util.Log
+import android.content.Context
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -26,6 +26,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -36,7 +37,10 @@ import com.kerikir.notes.presentation.utils.DateFormatter
 @Composable
 fun CreateNoteScreen(
     modifier: Modifier = Modifier,
-    viewModel: CreateNoteViewModel = viewModel(),
+    context: Context = LocalContext.current.applicationContext,
+    viewModel: CreateNoteViewModel = viewModel {
+        CreateNoteViewModel(context)
+    },
     onFinished: () -> Unit
 ) {
 
