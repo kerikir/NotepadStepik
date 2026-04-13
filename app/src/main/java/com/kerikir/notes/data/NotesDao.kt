7d9781b58914ed7e1d75a68148e547430ee48322
kerikir <vlasov-kerik@mail.ru>
2026-04-13 14:31:07 +0300

    added queries to the database

--- app/src/main/java/com/kerikir/notes/data/NotesDao.kt
+++ app/src/main/java/com/kerikir/notes/data/NotesDao.kt
@@ -1,7 +1,15 @@
 package com.kerikir.notes.data
 
 import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NotesDao {
+
+    @Query("SELECT * FROM notes ORDER BY updatedAt DESC")
+    fun getAllNotes(): Flow<List<NoteDbModel>>
+
+    @Query("SELECT * FROM notes WHERE title LIKE '%' || :query || '%' OR content LIKE '%' || :query || '%' ORDER BY updatedAt DESC")
+    fun searchNotes(query: String): Flow<List<NoteDbModel>>
 }
