7e82093371ff2c490ee914febd91df33a739185d
kerikir <vlasov-kerik@mail.ru>
2026-04-13 20:08:36 +0300

    added the database

---
+++ app/src/main/java/com/kerikir/notes/data/NotesDatabase.kt
@@ -0,0 +1,14 @@
+package com.kerikir.notes.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [NoteDbModel::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class NotesDatabase : RoomDatabase() {
+
+    abstract fun notesDao(): NotesDao
+}
