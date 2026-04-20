645c051e256847da5e406f277441c03a1a559ca4
kerikir <vlasov-kerik@mail.ru>
2026-04-20 17:43:22 +0300

    added getting a ViewModel instance from Hilt

--- app/src/main/java/com/kerikir/notes/presentation/screens/editing/EditNoteScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/editing/EditNoteScreen.kt
@@ -2,7 +2,6 @@
 
 package com.kerikir.notes.presentation.screens.editing
 
-import android.content.Context
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -27,22 +26,22 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.kerikir.notes.presentation.utils.DateFormatter
 
 @Composable
 fun EditNoteScreen(
     modifier: Modifier = Modifier,
     noteId: Int,
-    context: Context = LocalContext.current.applicationContext,
-    viewModel: EditNoteViewModel = viewModel {
-        EditNoteViewModel(noteId, context)
-    },
+    viewModel: EditNoteViewModel = hiltViewModel(
+        creationCallback = { factory: EditNoteViewModel.Factory ->
+            factory.create(noteId)
+        }
+    ),
     onFinished: () -> Unit
 ) {
 
