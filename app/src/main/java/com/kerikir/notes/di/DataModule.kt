b317ea5cd43819c2bf77c11645f6d1bba116b68a
kerikir <vlasov-kerik@mail.ru>
2026-04-20 16:46:11 +0300

    added the Module for the Hilt

---
+++ app/src/main/java/com/kerikir/notes/di/DataModule.kt
@@ -0,0 +1,10 @@
+package com.kerikir.notes.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DataModule {
+}
