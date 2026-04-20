00510f7d651a18318c57c9c5104701cb57d1ca4c
kerikir <vlasov-kerik@mail.ru>
2026-04-20 17:14:05 +0300

    added the dependencies in the ViewModels

--- app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesViewModel.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesViewModel.kt
@@ -10,6 +10,7 @@ import com.kerikir.notes.domain.GetAllNotesUseCase
 import com.kerikir.notes.domain.Note
 import com.kerikir.notes.domain.SearchNotesUseCase
 import com.kerikir.notes.domain.SwitchPinnedStatusUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -18,21 +19,19 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-
-class NotesViewModel(context: Context) : ViewModel() {
-
-    private val repository = NotesRepositoryImpl.getInstance(context)
-
-    private val getAllNotesUseCase = GetAllNotesUseCase(repository)
-    private val searchNotesUseCase = SearchNotesUseCase(repository)
-    private val switchPinnedStatusUseCase = SwitchPinnedStatusUseCase(repository)
+@HiltViewModel
+class NotesViewModel @Inject constructor(
+    private val getAllNotesUseCase: GetAllNotesUseCase,
+    private val searchNotesUseCase: SearchNotesUseCase,
+    private val switchPinnedStatusUseCase: SwitchPinnedStatusUseCase
+) : ViewModel() {
 
     private val query = MutableStateFlow("")
 
     private val _state = MutableStateFlow(NotesScreenState())
     val state = _state.asStateFlow()
-
 
 
     init {
@@ -54,7 +53,6 @@ class NotesViewModel(context: Context) : ViewModel() {
             }
             .launchIn(viewModelScope)
     }
-
 
 
     fun processCommand(command: NotesCommand) {
