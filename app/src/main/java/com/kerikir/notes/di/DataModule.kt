cb55b71e2f8df912e4b889e097567f8f9ecff254
kerikir <vlasov-kerik@mail.ru>
2026-04-20 16:52:44 +0300

    added the provide and bind methods in the Module

--- app/src/main/java/com/kerikir/notes/di/DataModule.kt
+++ app/src/main/java/com/kerikir/notes/di/DataModule.kt
@@ -1,10 +1,36 @@
 package com.kerikir.notes.di
 
+import android.content.Context
+import com.kerikir.notes.data.NotesDatabase
+import com.kerikir.notes.data.NotesRepositoryImpl
+import com.kerikir.notes.domain.NotesRepository
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface DataModule {
+
+    @Singleton
+    @Binds
+    fun bindNotesRepository(
+        impl: NotesRepositoryImpl
+    ): NotesRepository
+
+
+    companion object {
+
+        @Singleton
+        @Provides
+        fun provideDatabase(
+            @ApplicationContext context: Context
+        ): NotesDatabase {
+            return NotesDatabase.getInstance(context)
+        }
+    }
 }
