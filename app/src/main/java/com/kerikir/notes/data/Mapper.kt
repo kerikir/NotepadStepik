998727ab5c3da1f731effe575d95c7e6e9ec64c2
kerikir <vlasov-kerik@mail.ru>
2026-04-22 18:54:17 +0300

    added the mappers for convert ContentItems

--- app/src/main/java/com/kerikir/notes/data/Mapper.kt
+++ app/src/main/java/com/kerikir/notes/data/Mapper.kt
@@ -1,9 +1,38 @@
 package com.kerikir.notes.data
 
+import com.kerikir.notes.domain.ContentItem
 import com.kerikir.notes.domain.Note
 
 fun Note.toDbModel(): NoteDbModel {
     return NoteDbModel(id, title, content, updatedAt, isPinned)
+}
+
+
+fun List<ContentItem>.toContentItemsDbModel(): List<ContentItemDbModel> {
+    return map { contentItem ->
+        when (contentItem) {
+            is ContentItem.Image -> {
+                ContentItemDbModel.Image(url = contentItem.url)
+            }
+            is ContentItem.Text -> {
+                ContentItemDbModel.Text(content = contentItem.content)
+            }
+        }
+    }
+}
+
+
+fun List<ContentItemDbModel>.toContentItems(): List<ContentItem> {
+    return map { contentItem ->
+        when (contentItem) {
+            is ContentItemDbModel.Image -> {
+                ContentItem.Image(url = contentItem.url)
+            }
+            is ContentItemDbModel.Text -> {
+                ContentItem.Text(content = contentItem.content)
+            }
+        }
+    }
 }
 
 
