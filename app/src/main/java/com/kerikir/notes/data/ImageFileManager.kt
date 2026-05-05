c3c0a41cbd614d24827a798bb43a3c3d9b536c3c
kerikir <vlasov-kerik@mail.ru>
2026-05-05 15:29:19 +0300

    added the method copyImageToInternalStorage

--- app/src/main/java/com/kerikir/notes/data/ImageFileManager.kt
+++ app/src/main/java/com/kerikir/notes/data/ImageFileManager.kt
@@ -1,10 +1,29 @@
 package com.kerikir.notes.data
 
 import android.content.Context
+import androidx.core.net.toUri
 import dagger.hilt.android.qualifiers.ApplicationContext
+import java.io.File
+import java.util.UUID
 import javax.inject.Inject
 
 class ImageFileManager @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
+
+    private val imagesDir: File = context.filesDir
+
+
+    fun copyImageToInternalStorage(url: String): String {
+
+        val fileName = "IMG_${UUID.randomUUID()}.jpg"
+        val file = File(imagesDir, fileName)
+
+        val inputStream = context.contentResolver.openInputStream(url.toUri())
+        val outputStream = file.outputStream()
+
+        inputStream?.copyTo(outputStream)
+
+        return file.absolutePath
+    }
 }
