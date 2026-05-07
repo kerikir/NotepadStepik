64eeb114b5b5e26dfbcaefb8709455170d9cc6da
kerikir <vlasov-kerik@mail.ru>
2026-05-07 17:54:18 +0300

    deleted the unnecessary code

--- app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/kerikir/notes/presentation/screens/notes/NotesScreen.kt
@@ -33,13 +33,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -381,27 +379,5 @@ fun NoteCardWithImage(
                     fontWeight = FontWeight.Medium
                 )
             }
-    }
-}
-
-
-
-@Preview
-@Composable
-fun Gradient() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(
-                brush = Brush.verticalGradient(
-                    listOf(
-                        Color.Blue,
-                        Color.Green,
-                        Color.Red
-                    )
-                )
-            )
-    ) {
-
     }
 }
