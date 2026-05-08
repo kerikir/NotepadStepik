e008f82b2ec3a197ca703d736f4d2dd5bf6a47dd
kerikir <vlasov-kerik@mail.ru>
2026-05-08 14:30:29 +0300

    added library Splash Screen API in the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.coil.compose)
     implementation(libs.coil.network)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
     implementation(libs.hilt.navigation)
