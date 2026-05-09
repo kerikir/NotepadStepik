818ac1a65e6b483e14f914580e6571e1b8c5d4b3
kerikir <vlasov-kerik@mail.ru>
2026-05-09 15:51:21 +0300

    deleted the unnecessary code

--- app/src/main/java/com/kerikir/notes/data/ContentItemDbModel.kt
+++ app/src/main/java/com/kerikir/notes/data/ContentItemDbModel.kt
@@ -1,13 +1,2 @@
 package com.kerikir.notes.data
 
-import kotlinx.serialization.Serializable
-
-@Serializable
-sealed interface ContentItemDbModel {
-
-    @Serializable
-    data class Text(val content: String): ContentItemDbModel
-
-    @Serializable
-    data class Image(val url: String): ContentItemDbModel
-}
