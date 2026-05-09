8ff024a18c1026d225d78c473ee8b6d388c3c83a
kerikir <vlasov-kerik@mail.ru>
2026-05-09 16:09:23 +0300

    added the data class Content Item for the database

--- app/src/main/java/com/kerikir/notes/data/ContentItemDbModel.kt
+++ app/src/main/java/com/kerikir/notes/data/ContentItemDbModel.kt
@@ -1,5 +1,29 @@
 package com.kerikir.notes.data
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+
+
+@Entity(
+    tableName = "content",
+    primaryKeys = ["noteId", "order"],
+    foreignKeys = [
+        ForeignKey(
+            entity = NoteDbModel::class,
+            parentColumns = ["id"],
+            childColumns = ["noteId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class ContentItemDbModel(
+    val noteId: Int,
+    val contentType: ContentType,
+    val content: String,
+    val order: Int
+)
+
+
 enum class ContentType {
 
     TEXT, IMAGE
