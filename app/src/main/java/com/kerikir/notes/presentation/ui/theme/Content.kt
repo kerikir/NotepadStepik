574767040348fa94f94271c80b5dbf5b418a17c1
kerikir <vlasov-kerik@mail.ru>
2026-05-10 16:37:57 +0300

    a string resource with localization has been added

--- app/src/main/java/com/kerikir/notes/presentation/ui/theme/Content.kt
+++ app/src/main/java/com/kerikir/notes/presentation/ui/theme/Content.kt
@@ -22,10 +22,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
+import com.kerikir.notes.R
 import com.kerikir.notes.domain.ContentItem
 
 @Composable
@@ -113,18 +115,19 @@ private fun ImageContent(
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(8.dp)),
             model = imageUrl,
-            contentDescription = "Image from gallery",
+            contentDescription = stringResource(R.string.image_from_gallery),
             contentScale = ContentScale.FillWidth
         )
         Icon(
-            modifier = Modifier.align(Alignment.TopEnd)
+            modifier = Modifier
+                .align(Alignment.TopEnd)
                 .padding(8.dp)
                 .size(24.dp)
                 .clickable {
                     onDeleteImageClick()
                 },
             imageVector = Icons.Default.Close,
-            contentDescription = "Remove image",
+            contentDescription = stringResource(R.string.remove_image),
             tint = MaterialTheme.colorScheme.onSurface
         )
     }
@@ -156,7 +159,7 @@ private fun TextContent(
         ),
         placeholder = {
             Text(
-                text = "Note something down",
+                text = stringResource(R.string.note_something_down),
                 fontSize = 16.sp,
                 color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
             )
